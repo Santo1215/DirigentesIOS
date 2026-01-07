c323b214055c5b717e2fe3f00b638af4212b61c0
Santo1215 <jesus2221922@correo.uis.edu.co>
2026-01-07 11:10:14 -0500

    #1 Arreglos para el backend

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+const DATABASE_URL = 'postgresql://neondb_owner:npg_GS0OH3AmwxlW@ep-small-forest-afkx2784-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
--- backend/src/config/api.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://dirigentes-backend.up.railway.app';
